5dc3f34271813bd7bcbb69af9b0ca464701bda6f
AngusRenugounder <161382565+AngusRenugounder@users.noreply.github.com>
2024-02-27 10:20:19 +1300

    Create game.js
    initial commit

---
+++ game.js
@@ -0,0 +1,51 @@
+/*******************************************************/
+// P5.play: Geometry Dash Game
+// Create a sprite
+// Written by Angus
+/*******************************************************/
+console.log("%c t01_create_sprite", "color: blue;");
+const SCREEN_HEIGHT = 200;
+const SCREEN_WIDTH = 400;
+const PLAYER_HEIGHT = 25;
+const PLAYER_WIDTH = 25;
+/*******************************************************/
+// setup()
+/*******************************************************/
+function setup() {
+  console.log("setup: ");
+  cnv = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+world.gravity.y = 10;  
+rectangle = new Sprite(PLAYER_WIDTH, SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd'); 
+rectangle.color = 'cyan';
+wallBot  = new Sprite(0, SCREEN_HEIGHT, SCREEN_WIDTH*2, 10, 'k');
+wallBot.color = color("blue");
+
+document.addEventListener("keydown", function(event) {
+ if (event.code === 'ArrowUp') {
+rectangle.vel.y = -5;
+ }
+else if (event.code === 'ArrowDown')
+rectangle.vel.y = 10;
+});
+
+document.addEventListener("keyup", function(event) {
+if (event.code === 'ArrowUp') {
+   rectangle.vel.y = 0;
+ }
+
+else if (event.code === 'ArrowDown')
+rectangle.vel.y = 0;
+});
+}
+
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+background('lightgrey'); 
+
+}
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
