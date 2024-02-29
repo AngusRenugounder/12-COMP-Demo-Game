ed7d8bc6f30bfc7297dda56e7f5049d6914ca797
AngusRenugounder <161382565+AngusRenugounder@users.noreply.github.com>
2024-02-29 15:11:44 +1300

    Update game.js
    Working on obstacles

--- game.js
+++ game.js
@@ -8,18 +8,31 @@ const SCREEN_HEIGHT = 200;
 const SCREEN_WIDTH = 400;
 const PLAYER_HEIGHT = 25;
 const PLAYER_WIDTH = 25;
+const OBSTACLE_HEIGHT = 25;
+const OBSTACLE_WIDTH = 25;
+const OBSTACLE_SPAWN = 300;
+var spawnDist = 0+1;
+var obstacles;
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
   console.log("setup: ");
   cnv = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
-world.gravity.y = 80;  
+
 rectangle = new Sprite(PLAYER_WIDTH, SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd'); 
 rectangle.color = 'cyan';
-wallBot  = new Sprite(0, SCREEN_HEIGHT, SCREEN_WIDTH*2, 10, 'k');
-wallBot.color = color("blue");
 
+obstacle = new Sprite(OBSTACLE_SPAWN, SCREEN_HEIGHT - OBSTACLE_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'k'); 
+obstacle.color = 'yellow';
+obstacle.vel.x = -5
+
+obstacles = new Group();
+obstacles.add(obstacle);
+
+wallBot  = new Sprite(0, SCREEN_HEIGHT, SCREEN_WIDTH*2, 5, 'k');
+wallBot.color = color("blue");
+world.gravity.y = 80;  
 document.addEventListener("keydown", function(event) {
 
  if (event.code === 'ArrowUp' & rectangle.y > 175) {
@@ -39,16 +52,27 @@ if (event.code === 'ArrowUp') {
 else if (event.code === 'ArrowDown')
 rectangle.vel.y = 0;
 });
+rectangle.collides(obstacle, youDead);
 }
+
 
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
 background('lightgrey'); 
-
+newObstacle();
 }
-
+function newObstacle(){
+obstacle = new Sprite(OBSTACLE_SPAWN, SCREEN_HEIGHT - OBSTACLE_HEIGHT/2, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k'); 
+obstacle.color = 'yellow';
+obstacle.vel.x = -5    
+obstacles.add(obstacle);
+ spawnDist=spawnDist + 100;
+}
+function youDead(_rectangle, _obstacle){
+    console.log("YouDied")
+}
 /*******************************************************/
 //  END OF APP
 /*******************************************************/
